18fd47924f4f7a33db0296a2c89be71533105fe7
RaniaHathout <rania.hathout@pharma.asu.edu.eg>
2026-09-18 14:42:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # Streamlit safely reads the secret key you paste later in settings
-genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+genai.configure(api_key=st.secrets["GEMINI_API_KEY1"])
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 st.title("My AI Assistant")
